aa61fd59193a3c3a0297a7bae369668e292b6f61
gahmble <monishpatel189@gmail.com>
2024-08-09 15:22:06 -0400

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages =["Curating Discord Servers"];
+const statusMessages =["Discord Servers"];
 
 
 let currentIndex = 0;
@@ -73,7 +73,7 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    activities: [{ name: currentStatus, type: ActivityType.WATCHING}],
     status: 'dnd',
   });
 
